1f9bcca1e1e4c90a53af38169dff1c72aa6f85a8
Randel M <Randel.C.Moore@gmail.com>
2021-02-12 10:51:54 -0800

    add handleAddingNewSnakeToList function to SnakeControl

--- src/components/SnakeControl.js
+++ src/components/SnakeControl.js
@@ -5,11 +5,20 @@ class SnakeControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      formVisibleOnPage: false,
+      masterSnakeList: []
     };
   }
 
   //handleTaskFunctions
+
+  handleAddingNewSnakeToList = (newSnake) => {
+    const newMasterSnakeList = this.state.masterSnakeList.concat(newSnake);
+    this.setState({
+      masterSnakeList: newMasterSnakeList,
+      formVisibleOnPage: false
+    });
+  }
 
   render() {
     //If else conditionals
---
+++ src/components/SnakeForm.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function SnakeForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit = {props.formSubmissionHandler}>
+        <input
+        type = "text"
+        species = "species"
+        placeholder = "Species Name" />
+        <input
+          type = "text"
+          origin = "origin"
+          placeholder = "Origin" />
+        <input
+          type = "text"
+          temperment = "temperment"
+          placeholder = "Temperment" />
+        <input
+          type = "text"
+          lethality = "lethality"
+          placeholder = "Lethality" />
+        <button type = "submit"> {props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+SnakeForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default SnakeForm;
