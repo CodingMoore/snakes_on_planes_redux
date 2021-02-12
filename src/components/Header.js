8f07c5c1d7da7d5c6a14882a4fe89300b0442750
Randel M <Randel.C.Moore@gmail.com>
2021-02-12 09:50:20 -0800

    add basic structure for SnakeControl

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,2 +1,13 @@
 import React from "react";
 
+function Header() {
+  return (
+    <React.Fragment>
+      <h1>Snakes on Planes</h1>
+      <h2>Are your planes short on deadly reptiles?</h2>
+      <h2>Purchase Assassin-Snakes now!</h2>
+    </React.Fragment>
+  )
+}
+
+export default Header;
---
+++ src/components/SnakeControl.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+class SnakeControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  //handleTaskFunctions
+
+  render() {
+    //If else conditionals
+
+
+    return (
+      <React.Fragment>
+
+      </React.Fragment>  
+    );
+  }
+
+
+}
+
+export default SnakeControl;
+
