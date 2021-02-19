706737f98a7417dc19318890ccd5467e28f9cb73
Randel M <Randel.C.Moore@gmail.com>
2021-02-19 10:21:25 -0800

    create delete snake test

--- src/__tests__/reducers/master-snake-List-reducer.test.js
+++ src/__tests__/reducers/master-snake-List-reducer.test.js
@@ -12,7 +12,26 @@ describe("masterSnakeListReducer", () => {
     inventory: "Inventory Test",
     id: "ID Test"
   }
-  
+
+  const snakeListData = {
+    1: {
+    species: "Species 1",
+    nativeTo: "NativeTo 1",
+    description: "Description 1",
+    danger: "Danger 1",
+    inventory: "Inventory 1",
+    id: 1
+    },
+
+    2: {
+    species: "Species 2",
+    nativeTo: "NativeTo 2",
+    description: "Description 2",
+    danger: "Danger 2",
+    inventory: "Inventory 2",
+    id: 2
+    }
+  } 
 
   test("Should return the default state if there is no action type passed into the reducer", () => {
     expect(masterSnakeListReducer({}, { type: null})).toEqual({});
@@ -39,6 +58,26 @@ describe("masterSnakeListReducer", () => {
         inventory,
         id
         }
+      }
+    )
+  });
+
+  test("Should successfully delete the specified snake from the masterSnakeList", () => {
+    const action = {
+      type: "DELETE_SNAKE",
+      id: 1
+    }
+  
+    expect(masterSnakeListReducer(snakeListData, action)).toEqual(
+      {
+        2: {
+          species: "Species 2",
+          nativeTo: "NativeTo 2",
+          description: "Description 2",
+          danger: "Danger 2",
+          inventory: "Inventory 2",
+          id: 2
+          }
       }
     )
   })
