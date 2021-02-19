f09edfb90c65dfbd4d37006b196a7d03a24d2daf
Randel M <Randel.C.Moore@gmail.com>
2021-02-19 15:37:06 -0800

    update root reducer test to get pass with seed data

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -7,9 +7,49 @@ let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
   
+  // test("Should return default state if no action type is input", () => {
+  //   expect(rootReducer({}, { type: null })).toEqual({
+  //     masterSnakeList: {}
+  //   });
+  // });
+
   test("Should return default state if no action type is input", () => {
     expect(rootReducer({}, { type: null })).toEqual({
-      masterSnakeList: {}
+      masterSnakeList: 
+        {
+          1: {
+            species: "Whoop-Asp",
+            nativeTo: "Local Watering Holes",
+            description: "Have chips on their 'shoulders'",
+            danger: "They are only dangerous if you look at them 'funny'.",
+            inventory: 144,
+            id: "1"
+          },
+          2: {
+            species: "American Plissken",
+            nativeTo: "You used to find them in New York and LA",
+            description: "Has trouble with depth perception",
+            danger: "Highly dangerous (and cynical)",
+            inventory: 144,
+            id: "3"
+          },
+          3: {
+            species: "(gym)Rat-Snake",
+            nativeTo: "Venice Beach",
+            description: "'Do you even hiss Broa?'",
+            danger: "Lady Killers",
+            inventory: 144,
+            id: "2"
+          },
+          4: {
+            species: "Kojiman Solid Snake",
+            nativeTo: "Cardboard Boxes",
+            description: "Can be identified by a grey band on its head",
+            danger: "Extreme, known to hunt others of their own kind.",
+            inventory: 144,
+            id: "4"
+          }
+      }
     });
   });
 })
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
 import masterSnakeListReducer from "./master-snake-List-reducer";
-// import masterSnakeListReducer from "./master-snake-List-reducer";
+import seedReducer from "./seed-reducer";
 
 const rootReducer = combineReducers({
-  masterSnakeList: masterSnakeListReducer
+  masterSnakeList: masterSnakeListReducer,
+  masterSnakeList: seedReducer
 });
 
 export default rootReducer;
