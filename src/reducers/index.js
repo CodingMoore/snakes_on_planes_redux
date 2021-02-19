85701f0dbc40511737950e9f1dee6c1430057b1d
Randel M <Randel.C.Moore@gmail.com>
2021-02-19 14:16:18 -0800

    pass rootReducer test

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux";
+import masterSnakeListReducer from "./master-snake-List-reducer";
 // import masterSnakeListReducer from "./master-snake-List-reducer";
 
 const rootReducer = combineReducers({
-
+  masterSnakeList: masterSnakeListReducer
 });
 
 export default rootReducer;
